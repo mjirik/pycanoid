9af0dcef0cd2afffebb7454ca3970b7b940a30c8
somapatrik <soma.patrik@gmail.com>
2013-10-15 10:36:21 +0200

    Face detector

--- facecontrol.py
+++ facecontrol.py
@@ -11,7 +11,46 @@ class Facecontrol:
     def __init__(self):
 # default calibration_params
         self.calibration_params = 1
-        pass
+        self.cam_source = 1      # 0 - default, 1 - external
+        self.cam = cv2.VideoCapture(self.cam_source)
+        self.hc = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+        #cv2.resizeWindow("Webcam screen", 640, 480)
+
+
+
+
+
+    def get_image(self, cam):
+        ret, img = cam.read()
+        if ret:
+            gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            return gimg
+        else:
+            print "Error: No input"
+
+
+    def detect(self, img):
+        faces = self.hc.detectMultiScale(img)
+        for face in faces:
+            cv2.rectangle(img, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), (255, 0, 0), 3)
+        return faces
+
+
+    def face_center(self, faces):
+        positions = []
+
+        for face_pos in faces:
+            fpos = [face_pos[0] + (face_pos[2] / 2), face_pos[1] + (face_pos[3] / 2)]
+            positions.append(fpos)
+        return positions
+
+
+
+
+
+
+
+
     def set_calibration_params(self, calibration_params):
         self.calibration_params = calibration_params
 
@@ -22,11 +61,31 @@ class Facecontrol:
         position = position_precalibration
         return position
 
+
+
+
+
+
+
+
+
     def get_pos(self):
-        position_pc= [0 , 0 , 0]
 
-        position = self.__calibration(position_pc)
+        img = self.get_image(self.cam)
+        faces = self.detect(img)
 
-        return position
+        position_pc = [0 , 0 ]
+        if len(faces) > 0:
+            positions = self.face_center(faces)
+
+            position_pc = positions[0]
+            print 'Faces:', len(faces)
+
+
+
+
+        pos = self.__calibration(position_pc)
+
+        return pos
         pass
 
