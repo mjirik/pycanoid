5bc06a700638b09f1d8bb4a10602ec5ca1a022f6
tstation <miroslav.jirik+tstation@gmail.com>
2014-03-05 15:28:53 +0100

    get data from kinect

--- kinectcontrol.py
+++ kinectcontrol.py
@@ -14,36 +14,72 @@ from autobahn.websocket import WebSocketClientProtocol
 from autobahn.websocket import connectWS
 
 
-host = "ws://148.228.186.59:9002" 
+#host = "ws://148.228.186.59:9002" 
 host = "ws://localhost:9002" 
 
-data = None
+data = {"LeftHand":{"X":0, "Y":0}}
+
+kinectClientInstance = None
 
 class KinectControl():
-#   def 
-#    def get_pos()
+    
+   def __init__(self):
+      self.factory = WebSocketClientFactory(host, debug = False)
+      self.factory.protocol = KinectClient
+      connectWS(self.factory)
+      print "KC init"
+      print kinectClientInstance
+      print dir(kinectClientInstance)
+      
+    
+    #reactor.run()
+
+    
+   def get_pos(self):
+       
+       positionX = data["LeftHand"]['X']
+       positionY = data["LeftHand"]['Y']
+       position = (positionX, positionY)
+       
+       #self.factory.protocol.sendSkeletonRequest()
+       return position
 
 class KinectClient(WebSocketClientProtocol):
     
     def sendSkeletonRequest(self):
+        print "send skeleton request"
         self.sendMessage("skeleton")
         
     def onMessage(self, msg, binary):
-        
-        if len(msg) > 0:
+        print "on message"
+        if len(msg) > 2:
             self.data = json.loads(msg)
-            print self.data[0]['Head']
-            data = self.data
+            data = self.data[0]
+            print data
 
     def onOpen(self):
         self.sendSkeletonRequest()
-        
+        #self.data = None
+        print "on open"
+        #kinectClientInstance = self
+        print "on open"
         
 if __name__ == "__main__":
-    #pygame.init()
+    #kinect = KinectControl()
     factory = WebSocketClientFactory(host, debug = False)
     factory.protocol = KinectClient
-    print "sdfas"
     connectWS(factory)
-    print "sdfas"
+    print "run"
     reactor.run()
+    
+    #print kinect.get_pos()
+    
+   #while True:
+    #    print kinect.get_pos()
+#    #pygame.init()
+ #   factory = WebSocketClientFactory(host, debug = False)
+  #  factory.protocol = KinectClient
+   # print "sdfas"
+    #connectWS(factory)
+   # print "sdfas"
+   # reactor.run()
